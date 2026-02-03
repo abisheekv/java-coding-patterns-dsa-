d71e82b2ba1d5e915530319a85cba8b039f5f55a
abisheekv <abisheekv2006@gmail.com>
2026-02-03 20:11:15 +0530

    Added Java coding patterns

--- pattern0.java
+++ pattern0.java
@@ -1,10 +1,11 @@
-public class Main {
+public class pattern0 {
 	public static void main(String[] args) {
 		System.out.println("Hello, World!");
 		for(int i=1;i<=5;i++){
 			for(int j =1;j<=i;j++){
 				System.out.print(i + " ");
 			}
+			System.out.println();
 		}
 	}
 }
---
+++ pattern7.java
@@ -0,0 +1,6 @@
+public class pattern7 {
+	public static void main(String[] args) {
+		
+	}
+	
+}
