d552aa1724c5eabd8eaeadb64e2879f319bf20a4
abisheekv <abisheekv2006@gmail.com>
2026-02-03 20:08:57 +0530

    Added Java coding patterns

---
+++ pattern5.java
@@ -0,0 +1,9 @@
+public class pattern5 {
+	public static void main(String[] args) {
+		for(int i=0;i<5;i++){
+			for(int j=1;j<=5-i;j++){
+				System.out.print(j);
+			}System.out.println();
+		}
+	}
+}
