0a0f26220ec8c43aa469142552acf1e672853035
abisheekv <abisheekv2006@gmail.com>
2026-02-03 22:55:11 +0530

    Added Java coding patterns

---
+++ pattern10.java
@@ -0,0 +1,18 @@
+public class pattern10 {
+	public static void main(String[] args) {
+		for(int i=0;i<=4;i++){
+
+			for(int j=0;j<=i;j++){
+				System.out.print("* ");
+			}
+			System.out.println();
+
+		}
+		for(int k=0;k<=3;k++){
+			for(int l=0;l<4-k;l++){
+				System.out.print("*  ");
+			}
+			System.out.println();
+		}
+	}
+}
