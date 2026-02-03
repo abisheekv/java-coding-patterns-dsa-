7da52a6561f8ec8818af7b0190b5fd04b879f4ef
abisheekv <abisheekv2006@gmail.com>
2026-02-03 22:03:39 +0530

    Added Java coding patterns

--- pattern7.java
+++ pattern7.java
@@ -1,6 +1,20 @@
 public class pattern7 {
 	public static void main(String[] args) {
-		
+		int n=7;
+		for(int i=0;i<=4;i++){
+			for(int j=0;j<=n-1-i;j++){
+				System.out.print(" ");
+
+
+			}
+			for(int k=1;k<=2*i+1;k++){
+				System.out.print("* ");
+			}
+			for(int l=0;l<=n-1-i;l++){
+				System.out.print("");
 	}
+	System.out.println();
 	
+}
+	}
 }
